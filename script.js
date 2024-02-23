4343fde59f9ed4dea2cffb245198aa405cdbd77d
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-23 18:18:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let btn = document.getElementsByTagName("input")[0]
+
+// btn.addEventListener("click", insert_Row)
+
 function insert_Row() {
-    //Write your code here
-  
-  
+    let box = document.getElementsByTagName("table")[0]
+    let newrow = document.createElement("tr")
+    newrow.innerHTML = `<td>New Cell1</td><td>New Cell2</td>`
+    box.prepend(newrow)
+
 }
